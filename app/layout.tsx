270f1f3437fe56c33cf10f8f3c4262441cb8be6e
monerro <xnamelesswastaken@gmail.com>
2025-12-20 20:59:11 +1100

    Update site metadata for title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,15 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "@decal | dec.al",
-  description: "Private Biolink developed with Next.js | @x05k on ig",
+  title: "my site bro Lmao  | utkn",
+  description: "nextjs site",
   icons: {
     icon: "https://cdn.discordapp.com/emojis/1310254577831903254.webp?size=128",
   },
   openGraph: {
-    title: "@decal | dec.al",
-    description: "Private Biolink developed with Next.js | @x05k on ig",
-    url: "https://dec.al",
+    title: "my site bro Lmao  | utkn",
+    description: "nextjs site",
+    url: "https://soundcloud.com/bleoodbath",
     images: [
       {
         url: "https://i.pinimg.com/736x/9f/c7/e3/9fc7e3ec06cd28eb55b3bc8a0663cb69.jpg", // Replace with your thumbnail URL
